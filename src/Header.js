02ab1093cc190e843381c86bebd0f89a9edb2e43
John Mieszkowski <jmm3@njit.edu>
2020-12-11 11:17:56 -0500

    String of lights added

---
+++ src/Header.js
@@ -0,0 +1,49 @@
+import './App.scss';
+
+const Header = () =>{
+    return (
+            <ul class="lightrope">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        )
+    
+}
+
+export default Header;
